7945705510a35a207a16da492baa41acb9422f58
Jacob Dolph <jacob.dolph702@gmail.com>
2020-05-24 11:23:35 -0600

    building component and pages folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component'
+import Homepage from './homepage.component';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
+
+  const HatsPage = () => (
+    <div>
+      <h1>
+        HATS PAGE
+      </h1>
+    </div>
+  )
+
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
