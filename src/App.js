53fd223aa846141af0f9eaffb87203741c07730c
Jacob Dolph <jacob.dolph702@gmail.com>
2020-05-24 14:24:34 -0600

    added a shop page and a preview component for the shop page

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/Homepage';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
+import ShopPage from './pages/ShopPage'
 function App() {
-
-  const HatsPage = () => (
-    <div>
-      <h1>
-        HATS PAGE
-      </h1>
-    </div>
-  )
 
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/MenuItem/index.jsx
+++ src/components/MenuItem/index.jsx
@@ -3,10 +3,10 @@ import { withRouter } from 'react-router-dom';
 import './styles.scss';
 
 
-const MenuItem = ({ title, image, size, history, match, linkUrl }) => (
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
     <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{
-            backgroundImage: `url(${image})`
+            backgroundImage: `url(${imageUrl})`
         }} />
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
