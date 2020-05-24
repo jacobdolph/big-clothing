73b368d52cf1f9926b0d12bb53f2434f46e0cdc6
Jacob Dolph <jacob.dolph702@gmail.com>
2020-05-24 11:24:49 -0600

    removed unused components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/Hompage';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
