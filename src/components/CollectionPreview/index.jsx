0f96f2cb39e955411b0537f89df8f04c4a81b597
Jacob Dolph <jacob.dolph702@gmail.com>
2020-05-24 14:51:24 -0600

    created the item component

--- src/components/CollectionPreview/index.jsx
+++ src/components/CollectionPreview/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './styles.scss';
-
+import CollectionItem from '../CollectionItem'
 
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                items.filter((item, idx) => idx < 4).map((item) => (
-                    <div key={item.id}>{item.name}</div>
+                items.filter((item, idx) => idx < 4).map(({ id, ...otherItemProps }) => (
+                    <CollectionItem key={id}{...otherItemProps} />
                 ))
             }
         </div>
